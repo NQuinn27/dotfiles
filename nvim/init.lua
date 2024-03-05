6756746cbd6b21a780871c65f057acad69ad2de5
Niall Quinn <niall.quinn.27@gmail.com>
2024-03-05 15:33:07 +0100

    update bg

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,14 @@ require("lazy-set")
 require("set")
 require("map")
 require("lazy").setup("plugins")
+
+local TERMCS = os.getenv("TERMCS")
+
+local background
+if TERMCS == "light" then
+    background = "light"
+else
+    background = "dark"
+end
+
+vim.o.background = background
