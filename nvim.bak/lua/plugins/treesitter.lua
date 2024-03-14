c5b23a17c3de87dfdd039a91df150e23c4842e31
Niall Quinn <niall.quinn.27@gmail.com>
2024-03-14 11:22:54 +0100

    Updated

--- nvim.bak/lua/plugins/treesitter.lua
+++ nvim.bak/lua/plugins/treesitter.lua
@@ -86,8 +86,7 @@ return {
         },
       },
     })
-    -- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-    -- ft_to_parser.mdx = "markdown_inline"
     vim.treesitter.language.register('markdown_inline', 'mdx')
+    vim.treesitter.language.register('markdown', 'md')
   end,
 }
