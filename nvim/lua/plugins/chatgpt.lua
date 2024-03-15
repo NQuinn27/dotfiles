1d1fec77ba77de307fb6794f3e976afdbed1e60d
Niall Quinn <niall.quinn.27@gmail.com>
2024-03-15 09:34:46 +0100

    Revert Lazy

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -5,13 +5,10 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  -- event = "VeryLazy",
   cmd = { "ChatGPT", "ChatGPTRun", "ChatGPTActAs", "ChatGPTEditWithInstructions" },
   config = function()
-    local home = vim.fn.expand("$HOME")
     require("chatgpt").setup({
-      api_key_cmd = "op read op://Personal/openAINvim/credential --no-newline",
-      -- api_key_cmd = "gpg --decrypt " .. home .. "/openAI.txt.gpg",
+      api_key_cmd = "op read op://Personal/openAINvim/password --no-newline"
     })
-  end,
+  end
 }
