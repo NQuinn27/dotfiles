6fa78c3580d12bccec6184a63d491e61e8b79238
Niall Quinn <niall.quinn.27@gmail.com>
2024-04-02 12:05:56 +0200

    Replace null_ls with nvim-lint and conform

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -1,5 +1,5 @@
--- vim.g.loaded_netrwPlugin = 1
--- vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -10,7 +10,7 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
-			close_if_last_window = true,
+			-- close_if_last_window = true,
 			window = {
 				position = "left"
 			}
