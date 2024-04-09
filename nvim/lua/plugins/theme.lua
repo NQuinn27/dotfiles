53627439f3cf6a1edb4cc2e5cdd50461239ef94b
Niall Quinn <niall.quinn.27@gmail.com>
2024-04-09 15:15:24 +0200

    Remove tree

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,26 +1,28 @@
 return {
-	-- "catppuccin/nvim", name = "catppuccin", priority = 1000,
-	-- config = function()
-	--   require("catppuccin").setup({
-	--     flavour = "mocha",
-	--     -- transparent_background = true,
-	--     background = {
-	--       dark = "mocha",
-	--       light = "latte"
-	--     }
-	--   })
-	--   vim.cmd.colorscheme "catppuccin"
-	-- end
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				style = "storm",
-				light_style = "day",
-			})
-			vim.cmd.colorscheme("tokyonight")
-		end,
-	},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			-- transparent_background = true,
+			background = {
+				dark = "mocha",
+				light = "latte",
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("tokyonight").setup({
+	-- 			style = "storm",
+	-- 			light_style = "day",
+	-- 		})
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 	end,
+	-- },
 }
