e6926fedfb6bcb2291493c4a0f2f7e8cadcbb4ca
Niall Quinn <niall@primer.io>
2025-01-16 16:12:23 +0100

    Tweaking filetree and adding oil.nvim

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -1,24 +1,25 @@
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			-- close_if_last_window = true,
-			window = {
-				position = "left",
-				auto_close = true,
-				auto_open = false,
-			},
-		})
-		vim.keymap.set("n", "<leader>]", ":Neotree toggle<CR>", {})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            -- close_if_last_window = true,
+            window = {
+                position = "left",
+                auto_close = true,
+                auto_open = false,
+            },
+        })
+        vim.keymap.set("n", "<leader>]", ":Neotree toggle<CR>", {})
+        vim.keymap.set("n", "<leader>[", ":Neotree toggle reveal float<CR>", {})
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
 }
