4666d4105cc3c34fbe890f919656078bf0fad5f7
Niall Quinn <niall.quinn.27@gmail.com>
2025-01-17 12:46:21 +0100

    MD formatting

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -12,6 +12,7 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
+        -- Set markdown-specific settings
         vim.opt.conceallevel = 2
         require("obsidian").setup({
             workspaces = {
@@ -30,7 +31,14 @@ return {
             },
             attachments = {
                 img_folder = "Z_Assets"
-            }
+            },
+            follow_url_func = function(url)
+                -- Open the URL in the default web browser.
+                vim.fn.jobstart({ "open", url }) -- Mac OS
+                -- vim.fn.jobstart({"xdg-open", url})  -- linux
+                -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+                -- vim.ui.open(url) -- need Neovim 0.10.0+
+            end,
         })
     end
 }
