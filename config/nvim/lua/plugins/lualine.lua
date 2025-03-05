a7f3fda9a9b735043eecf55a075e0f9ad140650e
Niall Quinn <niall.quinn.27@gmail.com>
2025-03-06 00:59:52 +0100

    fixed ftplugins

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -8,6 +8,15 @@ return {
 				component_separators = "|",
 				section_separators = "",
 			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
 		})
 	end,
 }
--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -7,6 +7,7 @@ return {
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
 	},
 	setup = function()
 		require("noice").setup({
@@ -25,6 +26,12 @@ return {
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+			routes = {
+				{
+					view = "notify",
+					filter = { event = "msg_showmode" },
+				},
 			},
 		})
 	end,
