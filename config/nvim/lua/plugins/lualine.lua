ba4e5f20eb2d66f63aa6c06c3098718137be68ed
Niall Quinn <niall.quinn.27@gmail.com>
2025-03-07 09:16:27 +0100

    Tweak tmux

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -9,12 +9,55 @@ return {
 				section_separators = "",
 			},
 			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+
+				lualine_c = {
+					{
+						"diagnostics",
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ "filename" },
+				},
 				lualine_x = {
 					{
 						require("noice").api.statusline.mode.get,
 						cond = require("noice").api.statusline.mode.has,
 						color = { fg = "#ff9e64" },
 					},
+					{
+						function()
+							return "  " .. require("dap").status()
+						end,
+						cond = function()
+							return package.loaded["dap"] and require("dap").status() ~= ""
+						end,
+						color = function()
+							return { fg = Snacks.util.color("Debug") }
+						end,
+					},
+					{
+						"diff",
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_y = {
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_z = {
+					function()
+						return " " .. os.date("%R")
+					end,
 				},
 			},
 		})
