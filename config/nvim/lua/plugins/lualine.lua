fa3213a13435378259bef9e75435054e7194001d
Niall Quinn <niall.quinn.27@gmail.com>
2025-03-11 10:48:42 +0100

    remove noice

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -21,11 +21,6 @@ return {
 				},
 				lualine_x = {
 					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { fg = "#ff9e64" },
-					},
-					{
 						function()
 							return "  " .. require("dap").status()
 						end,
