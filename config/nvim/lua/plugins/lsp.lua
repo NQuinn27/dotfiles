bd82134ba47366a6545714d76c31fc333a4693aa
Niall Quinn <niall@primer.io>
2025-03-15 10:17:39 +0100

    Fix cucumber lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -135,9 +135,16 @@ return {
 			on_attach = on_attach,
 			capabilities = capabilities,
 			cmd = { "cucumber-language-server", "--stdio" },
+			root_dir = require("lspconfig").util.find_git_ancestor,
 			runtime = {
 				version = "0.0.1",
 				path = "cucumber-language-server",
+			},
+			settings = {
+				cucumber = {
+					features = { "features/**/*.feature" },
+					glue = { "src/**/*.ts" },
+				},
 			},
 		})
 
