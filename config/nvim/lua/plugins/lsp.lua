46e9b5db9f3c5b7319af61f68e445bc4ccf23f65
Niall Quinn <niall@primer.io>
2025-04-03 21:36:47 +0200

    Fix cucumber

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,7 +13,11 @@ return {
       },
       cucumber_language_server = {
         cmd = { "cucumber-language-server", "--stdio" },
-        root_dir = vim.fs.dirname(vim.fs.find(".git", { path = ".", upward = true })[1]),
+        root_dir = require("lspconfig").util.find_git_ancestor,
+        runtime = {
+          version = "0.0.1",
+          path = "cucumber-language-server",
+        },
         settings = {
           cucumber = {
             features = { "features/**/*.feature" },
