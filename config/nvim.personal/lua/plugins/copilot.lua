ec914cbbef856faf28f7b116902e3294e7de7cd6
Niall Quinn <niall.quinn.27@gmail.com>
2025-04-03 21:23:51 +0200

    Updates

--- config/nvim.personal/lua/plugins/copilot.lua
+++ config/nvim.personal/lua/plugins/copilot.lua
@@ -14,7 +14,14 @@ return {
 		build = "make tiktoken",
 		opts = {
 			debug = true, -- Enable debugging
+			window = {
+				width = 0.35,
+			},
 			-- See Configuration section for rest
 		}, -- See Commands section for default commands if you want to lazy load on them
+		keys = {
+			{ "<leader>aa", ":CopilotChat<CR>", desc = "Open CopilotChat" },
+			{ "<leader>ac", ":CopilotChatClose<CR>", desc = "Close CopilotChat" },
+		},
 	},
 }
