d7e9b17817f67c8e9fc506242d9cba7ff796e458
Niall Quinn <niall.quinn.27@gmail.com>
2025-10-21 21:43:36 +0200

    Add disable autoformatting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,14 +3,4 @@ require("set")
 require("map")
 require("lazy").setup("plugins")
 
--- vim.cmd("colorscheme catppuccin")
--- local TERMCS = os.getenv("TERMCS")
 vim.g.mapleader = " "
--- local background
--- if TERMCS == "light" then
---     background = "light"
--- else
---     background = "dark"
--- end
---
--- vim.o.background = background
