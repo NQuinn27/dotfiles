6cfcfe54f8dd29134cdc1fa65f5b145af74daa76
Niall Quinn <niall@primer.io>
2025-10-21 21:43:36 +0200

    Add disable autoformatting

--- config/nvim/lua/plugins/linting_formatting.lua
+++ config/nvim/lua/plugins/linting_formatting.lua
@@ -15,15 +15,21 @@ return {
                 gherkin = { "gherkin-lint" },
             },
             format_on_save = function(bufnr)
-                if vim.bo[bufnr].filetype == "yaml" then
+                if vim.g.autoformat then
+                    local disable_filetypes = {}
+                    local lsp_format_opt
+                    if disable_filetypes[vim.bo[bufnr].filetype] then
+                        lsp_format_opt = "never"
+                    else
+                        lsp_format_opt = "fallback"
+                    end
+                    return {
+                        timeout_ms = 500,
+                        lsp_format = lsp_format_opt,
+                    }
+                else
                     return
                 end
-
-                -- These options will be passed to conform.format()
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = true,
-                }
             end,
         },
     },
@@ -42,6 +48,25 @@ return {
                 json = { "jsonlint" },
                 gherkin = { "gherkin-lint" },
             }
+        end,
+    },
+    { -- Assuming you init/setup snacks.nvim in a different file
+        "folke/snacks.nvim",
+        opts = function()
+            vim.g.autoformat = true -- Sets up the default value to be true
+
+            require("snacks").toggle
+                .new({
+                    id = "Format on Save",
+                    name = "Format on Save",
+                    get = function()
+                        return vim.g.autoformat
+                    end,
+                    set = function(_)
+                        vim.g.autoformat = not vim.g.autoformat
+                    end,
+                })
+                :map("<leader>uf")
         end,
     },
 }
