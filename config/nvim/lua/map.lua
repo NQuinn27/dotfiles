a9ce63c51d23d07f11024375ed0cd3236c591a00
Niall Quinn <niall.quinn.27@gmail.com>
2025-11-26 15:13:52 +0100

    re-add

---
+++ config/nvim/lua/map.lua
@@ -0,0 +1,46 @@
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "-", vim.cmd.Explore)
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Move whole lines up and down
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+vim.keymap.set("n", "Q", "<nop")
+
+-- Find and replace for current word under cursor
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "t<C-n>", vim.cmd.TestNearest, { noremap = true, silent = true })
+vim.keymap.set("n", "t<C-f>", vim.cmd.TestFile, { noremap = true, silent = true })
+vim.keymap.set("n", "t<C-f>", vim.cmd.TestSuite, { noremap = true, silent = true })
+vim.keymap.set("n", "t<C-s>", vim.cmd.Testlast, { noremap = true, silent = true })
+vim.keymap.set("n", "t<C-g>", vim.cmd.TestVisit, { noremap = true, silent = true })
+
+-- Switch panes sides
+-- vim.keymap.set("n", "<leader>j", "<C-w>R")
+-- vim.keymap.set("n", "<leader>k", "<C-w>L")
+
+vim.keymap.set("n", "<leader>mdp", vim.cmd.MarkdownPreview, { noremap = true, silent = true })
+-- Remap <C-d> to <C-d>zz
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
---
+++ config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"rust-lang/rust.vim",
+		ft = { "rust" },
+		config = function()
+			vim.g.rustfmt_autosave = 1
+			vim.g.rustfmt_emit_files = 1
+			vim.g.rustfmt_fail_silently = 0
+			vim.g.rust_clip_command = "wl-copy"
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = vim.fn.has("nvim-0.10.0") == 0 and "^4" or false,
+		ft = { "rust" },
+	},
+}
