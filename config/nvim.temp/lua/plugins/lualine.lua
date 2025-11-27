b7cd754043c574a19471eb49f4fad76a26f8fd88
Niall Quinn <niall.quinn.27@gmail.com>
2025-11-27 09:33:45 +0100

    Updated zsh config

---
+++ config/nvim.temp/lua/plugins/lualine.lua
@@ -0,0 +1,74 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "auto",
+                icons_enabled = true,
+                component_separators = "|",
+                section_separators = "",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+
+                lualine_c = {
+                    { "diagnostics" },
+                    {
+                        "filetype",
+                        icon_only = true,
+                        separator = "",
+                        padding = { left = 1, right = 0 },
+                    },
+                    { "filename" },
+                },
+                lualine_x = {
+                    {
+                        function()
+                            return "  " .. require("dap").status()
+                        end,
+                        cond = function()
+                            return package.loaded["dap"] and require("dap").status() ~= ""
+                        end,
+                        color = function()
+                            return { fg = Snacks.util.color("Debug") }
+                        end,
+                    },
+                    {
+                        "diff",
+                        source = function()
+                            local gitsigns = vim.b.gitsigns_status_dict
+                            if gitsigns then
+                                return {
+                                    added = gitsigns.added,
+                                    modified = gitsigns.changed,
+                                    removed = gitsigns.removed,
+                                }
+                            end
+                        end,
+                    },
+                },
+                lualine_y = {
+                    { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+                    { "location", padding = { left = 0, right = 1 } },
+                },
+                lualine_z = {
+                    function()
+                        return " " .. os.date("%R")
+                    end,
+                },
+            },
+        })
+
+        -- Update lualine theme when colorscheme changes
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = function()
+                require("lualine").setup({
+                    options = {
+                        theme = get_lualine_theme(),
+                    },
+                })
+            end,
+        })
+    end,
+}
---
+++ config/nvim.temp/lua/plugins/which-key.lua
@@ -0,0 +1,51 @@
+-- which-key helps you remember key bindings by showing a popup
+-- with the active keybindings of the command you started typing.
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		plugins = { spelling = true },
+		defaults = {},
+		spec = {
+			mode = { "n", "v" },
+			{ "g", group = "+goto" },
+			{ "gs", group = "+surround" },
+			{ "z", group = "+fold" },
+			{ "]", group = "+next" },
+			{ "[", group = "+prev" },
+			{ "<leader><tab>", group = "tabs" },
+			{ "<leader>b", group = "+buffer" },
+			{ "<leader>c", group = "+code" },
+			{ "<leader>f", group = "+file/find" },
+			{ "<leader>g", group = "+git" },
+			{ "<leader>gh", group = "+hunks" },
+			{ "<leader>q", group = "+quit/session" },
+			{ "<leader>s", group = "+search" },
+			{ "<leader>u", group = "+ui" },
+			{ "<leader>w", group = "+windows" },
+			{ "<leader>x", group = "+diagnostics/quickfix" },
+			{ "<leader>s", group = "+snacks" },
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Keymaps (which-key)",
+		},
+		{
+			"<c-w><space>",
+			function()
+				require("which-key").show({ keys = "<c-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode (which-key)",
+		},
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.add(opts.defaults)
+	end,
+}
