8d3e1b0137fe2b67ddde0f02f153eeb1f4f42561
Niall Quinn <niall.quinn.27@gmail.com>
2025-12-09 11:12:44 +0100

    Update

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -30,7 +30,21 @@ return {
       },
     },
   },
+  { "sainnhe/gruvbox-material", name = "gruvbox-material" },
   { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type solarized.config
+    opts = {},
+    config = function(_, opts)
+      vim.o.termguicolors = true
+      vim.o.background = "light"
+      require("solarized").setup(opts)
+      vim.cmd.colorscheme("solarized")
+    end,
+  },
   {
     "f-person/auto-dark-mode.nvim",
     lazy = false,
@@ -39,18 +53,20 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
-        vim.cmd("colorscheme catppuccin")
-        -- require("vscode").load("dark")
-        -- vim.api.nvim_set_hl(0, "Normal", { bg = "#1D1D1D" })
-        -- vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#D7BA7D", bg = "none", nocombine = true })
-        -- vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181818" })
+        -- vim.cmd("colorscheme catppuccin")
+        -- vim.cmd("colorscheme gruvbox-material")
+        require("vscode").load("dark")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#1D1D1D" })
+        vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#D7BA7D", bg = "none", nocombine = true })
+        vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181818" })
       end,
       set_light_mode = function()
         vim.o.background = "light"
+        -- vim.cmd("colorscheme gruvbox-material")
         require("vscode").load("light")
         vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#F8F8F8" })
         vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#90D5FF", bg = "#F8F8F8", nocombine = true })
