fd7285b104b8d8e3e59364e3ea6597b6386613eb
Niall Quinn <niall.quinn.27@gmail.com>
2025-12-16 12:00:55 +0100

    VSCODE

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -10,40 +10,41 @@ return {
       disable_nvimtree_bg = false,
     },
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      styles = {
-        keywords = { "italic" },
-      },
-      lsp_styles = {
-        inlay_hints = {
-          background = false,
-        },
-      },
-    },
-  },
   { "sainnhe/gruvbox-material", name = "gruvbox-material" },
   { "rose-pine/neovim", name = "rose-pine" },
   {
-    "maxmx03/solarized.nvim",
-    lazy = false,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
-    ---@type solarized.config
-    opts = {},
-    config = function(_, opts)
-      vim.o.termguicolors = true
-      vim.o.background = "light"
-      require("solarized").setup(opts)
-      vim.cmd.colorscheme("solarized")
-    end,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    },
   },
   {
     "f-person/auto-dark-mode.nvim",
@@ -53,16 +54,16 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
-        vim.cmd("colorscheme catppuccin-frappe")
-        -- vim.cmd("colorscheme gruvbox-material")
-        -- require("vscode").load("dark")
-        -- vim.api.nvim_set_hl(0, "Normal", { bg = "#1D1D1D" })
-        -- vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#D7BA7D", bg = "none", nocombine = true })
-        -- vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "#181818" })
-        -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181818" })
+        -- vim.cmd("colorscheme catppuccin-frappe")
+        vim.cmd("colorscheme gruvbox")
+        require("vscode").load("dark")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#1E2021" })
+        vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#D7BA7D", bg = "none", nocombine = true })
+        vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "#181818" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181818" })
       end,
       set_light_mode = function()
         vim.o.background = "light"
