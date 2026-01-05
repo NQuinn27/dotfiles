e84db49542f87cd93870be1628cbc2c425c5c632
Niall Quinn <niall.quinn.27@gmail.com>
2026-01-05 09:43:59 +0100

    Update completions

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,96 +1,43 @@
 return {
-	{
+	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSUpdate", "TSInstall" },
-		config = function()
-			-- Install parsers
-			require("nvim-treesitter").install({
+		main = "nvim-treesitter.config", -- Sets main module to use for opts
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		opts = {
+			ensure_installed = {
+				"bash",
 				"c",
-				"cpp",
-				"go",
+				"diff",
+				"html",
 				"lua",
-				"python",
-				"rust",
-				"ron",
-				"typescript",
-				"vim",
-				"ruby",
+				"luadoc",
 				"markdown",
 				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"rust",
+				"go",
 				"javascript",
-				"yaml",
-			})
-
-			-- Enable highlighting
-			vim.api.nvim_create_autocmd("FileType", {
-				callback = function(args)
-					pcall(vim.treesitter.start, args.buf)
-				end,
-			})
-
-			-- Register markdown for mdx/md
-			vim.treesitter.language.register("markdown", "mdx")
-			vim.treesitter.language.register("markdown", "md")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		event = { "BufReadPost", "BufNewFile" },
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			-- Textobjects select
-			local select_maps = {
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-			}
-			for keymap, query in pairs(select_maps) do
-				vim.keymap.set({ "x", "o" }, keymap, function()
-					require("nvim-treesitter-textobjects.select").select_textobject(query, "textobjects")
-				end)
-			end
-
-			-- Textobjects move
-			local ts_move = require("nvim-treesitter-textobjects.move")
-			vim.keymap.set({ "n", "x", "o" }, "]m", function()
-				ts_move.goto_next_start("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]]", function()
-				ts_move.goto_next_start("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "]M", function()
-				ts_move.goto_next_end("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "][", function()
-				ts_move.goto_next_end("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[m", function()
-				ts_move.goto_previous_start("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[[", function()
-				ts_move.goto_previous_start("@class.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[M", function()
-				ts_move.goto_previous_end("@function.outer", "textobjects")
-			end)
-			vim.keymap.set({ "n", "x", "o" }, "[]", function()
-				ts_move.goto_previous_end("@class.outer", "textobjects")
-			end)
-
-			-- Textobjects swap
-			local ts_swap = require("nvim-treesitter-textobjects.swap")
-			vim.keymap.set("n", "<leader>a", function()
-				ts_swap.swap_next("@parameter.inner")
-			end)
-			vim.keymap.set("n", "<leader>A", function()
-				ts_swap.swap_previous("@parameter.inner")
-			end)
-		end,
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
+		},
+		-- There are additional nvim-treesitter modules that you can use to interact
+		-- with nvim-treesitter. You should go explore a few and see what interests you:
+		--
+		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
 	{
 		"RRethy/nvim-treesitter-endwise",
