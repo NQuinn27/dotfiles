cc3cf8479d0f888a96d8394e35469174862c3a86
Niall Quinn <niall@primer.io>
2026-01-13 16:57:44 +0100

    Tweakz

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,6 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.g.mapleader = " "
+require("lazy-set")
+require("set")
+require("map")
+require("terminal")
+require("lazy").setup("plugins")
