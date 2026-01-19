ce564efb98264948f6e6fecc8ddf1984c420542e
Niall Quinn <niall.quinn.27@gmail.com>
2026-01-19 22:23:48 +0100

    Fixup colors

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,30 +1,5 @@
 return {
 	{
-		"Mofiqul/vscode.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = false,
-			italic_comments = true,
-			underline_links = true,
-			disable_nvimtree_bg = false,
-		},
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				variant = "main",
-				styles = {
-					bold = true,
-					italic = true,
-					transparency = true,
-				},
-			})
-		end,
-	},
-	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
@@ -74,16 +49,7 @@ return {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.o.background = "dark"
-				vim.cmd("colorscheme gruvbox")
-				-- require("vscode").load("dark")
-				-- -- Comments more prominent
-				-- vim.api.nvim_set_hl(0, "Normal", { bg = "#141415" })
-				vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "#1E2021" })
-				-- vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#D7BA7D", bg = "none", nocombine = true })
-				-- vim.api.nvim_set_hl(0, "SnacksNormalNC", { bg = "#181818" })
-				vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1E2021" })
-				-- vim.api.nvim_set_hl(0, "SnacksNormal", { bg = "#181818" })
-				-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#181818" })
+				vim.cmd("colorscheme catppuccin")
 			end,
 			set_light_mode = function()
 				vim.o.background = "light"
