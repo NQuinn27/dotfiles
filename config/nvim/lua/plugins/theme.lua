825b19acb4a3257897383062084c3aaea4ddd104
Niall Quinn <niall.quinn.27@gmail.com>
2026-07-22 14:04:32 +0200

    Catpp

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -72,11 +72,11 @@ return {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.o.background = "dark"
-				vim.cmd.colorscheme("everforest")
+				vim.cmd.colorscheme("catppuccin")
 			end,
 			set_light_mode = function()
 				vim.o.background = "light"
-				vim.cmd.colorscheme("everforest")
+				vim.cmd.colorscheme("catppuccin")
 			end,
 		},
 	},
