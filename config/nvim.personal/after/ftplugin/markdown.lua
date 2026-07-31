eb2e51a8e589d46596f8c910c36eb1182e3502cd
Niall Quinn <niall.quinn.27@gmail.com>
2026-07-31 16:06:02 +0200

    Remove old config

--- config/nvim.personal/after/ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
-vim.opt_local.conceallevel = 0
-vim.opt_local.textwidth = 120 -- Set text width to 120 characters
-vim.opt_local.wrap = true -- Enable line wrapping
-vim.opt_local.linebreak = true -- Wrap at word boundaries
